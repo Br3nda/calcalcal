9b0cd51be84148956ed4a4cbeb2750408f794ee8
Brenda Wallace <shiny@cpan.org>
2013-04-29 21:28:58 +1200

    fixed dbconnection

--- pylib/calcalcal/db.py
+++ pylib/calcalcal/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import Table, MetaData, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import mapper
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-engine = create_engine('pgsql://evencal@eventcal:', echo=True)
+engine = create_engine('postgresql+psycopg2://evencal@localhost:5432/eventcal:', echo=True)
 Base = declarative_base()
 
 from sqlalchemy.orm import sessionmaker
@@ -11,8 +11,8 @@ print DB
 
 class Group(Base):
     __tablename__ = 'groups'
-    
-    id = Column(Integer, primary_key=True)
+    id = Column('id', Integer, Sequence('group_id_seq'), primary_key=True)
+##    id = Column(Integer, primary_key=True)
     name = Column(String)
     ical_url = Column(String)
 
