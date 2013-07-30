098f2b053e1886eb66e56ce496b94ed188646a50
Brenda Wallace <shiny@cpan.org>
2013-07-30 21:54:14 +1200

    import Sequence

--- pylib/calcalcal/db.py
+++ pylib/calcalcal/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, MetaData, Column, ForeignKey, Integer, String
+from sqlalchemy import Table, MetaData, Column, ForeignKey, Integer, String, Sequence
 from sqlalchemy.orm import mapper
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
