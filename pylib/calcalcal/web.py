181593f618bb014b9eea98029000006d8e185676
Brenda Wallace <shiny@cpan.org>
2013-09-30 21:14:46 +1300

    added next month in json returned

--- pylib/calcalcal/web.py
+++ pylib/calcalcal/web.py
@@ -1,4 +1,5 @@
 import datetime
+import calendar
 import calcalcal
 def year_from_today():
     return {
@@ -24,7 +25,7 @@ def get_events():
       events_today.append({'id': e.id, 'name': e.name})
       
       
-    import calendar
+    
     last_day_of_month = calendar.monthrange(today.year ,today.month)[1]
     first_of_month = today.replace(day=1)
     end_of_month = today.replace(day=last_day_of_month)    
@@ -32,8 +33,27 @@ def get_events():
     for e in calcalcal.get_events(first_of_month, end_of_month):
       events_this_month.append({'id': e.id, 'name': e.name})
 
+    first_of_next_month = add_months(first_of_month, 1)
+    last_day_of_next_month = calendar.monthrange(first_of_next_month.year, first_of_next_month.month)[1]
+    last_of_next_month = first_of_next_month.replace(day=last_day_of_next_month)
+    events_next_month = []
+    for e in calcalcal.get_events(first_of_next_month, last_of_next_month):
+      events_next_month.append({'id': e.id, 'name': e.name})
+    
+    print first_of_next_month.month
+    next_month_name = first_of_next_month.strftime('%B')
     return {
 	'today': events_today,
         'this_month': events_this_month,
-        'next_month': []
+        'next_month': events_next_month,
+        'next_month_name': next_month_name
         }
+    
+    
+def add_months(sourcedate, months):
+    month = sourcedate.month - 1 + months
+    year = sourcedate.year + month / 12
+    month = month % 12 + 1
+    day = min(sourcedate.day, calendar.monthrange(year,month)[1])
+    added_months = datetime.date(year, month, day)    
+    return added_months
