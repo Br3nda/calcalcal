c3f0000b346396d2f6c77c2afc176414230435d6
Brenda Wallace <shiny@cpan.org>
2013-09-30 21:21:57 +1300

    begin to fill in page from returned json

--- www/js/eventcal.js
+++ www/js/eventcal.js
@@ -24,6 +24,26 @@ function get_next_events() {
         url: "/api/events.json",
         context: document.body
     }).done(function(response) {
-	console.log(response);
+	$('#section-calendar').empty().append('<h2>Today</h2>');
+	
+	for (i in response['today']) {
+	    e = response['today'][i];
+	    console.log(e);
+	    $('#section-calendar').append(event_markup(e));
+	}
     });
+}
+
+function event_markup(e) {
+  event = ' \
+    <div class="line-item"> \
+	    <span class="day">Tu</span> \
+	    <span class="date">4</span> \
+	    <span class="time">6:30pm</span> \
+	    <span class="group"><a href="#">Creative Skills</a></span> \
+	    <span class="event">The Smell of Ink in the Morning</span> \
+	    <span class="location">The Kitchen</span> \
+    </div> \
+    ';
+    return event
 }
